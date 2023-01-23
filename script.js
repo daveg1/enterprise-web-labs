419a2690d1cdaa4bebe158cc384e8cba4df395ea
David Graham <d.graham8@rgu.ac.uk>
2023-01-23 00:37:48 +0000

    refactor script

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-function calculatePrice() {
-	var s = document.getElementById('salary').value
-	var d = document.getElementById('days').value
-	console.log('Calculating price')
-	console.log(s)
-	console.log(d)
-	let finalPrice = 0
-	dailyRate = s / 365
-	finalPrice = dailyRate * d
-	document.getElementById('finalPrice').innerHTML = finalPrice
+window.onload = (e) => {
+	const formElem = document.querySelector('#budget-form')
+	const outputElem = document.querySelector('#price-output')
+
+	formElem.addEventListener('submit', (e) => {
+		e.preventDefault()
+
+		const form = new FormData(formElem)
+		const salary = form.get('salary')
+		const days = form.get('days')
+
+		console.log('Calculating price', salary, days)
+
+		const finalPrice = (salary / 365) * days
+		outputElem.textContent = finalPrice
+	})
 }
